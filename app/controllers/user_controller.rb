a46e6687259851c3003c76420329d29a14c6f18e
Juan Manuel Mougan <juanmougan@gmail.com>
2020-06-23 23:07:26 -0300

    Made it work

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,13 +1,12 @@
 class UserController < ApplicationController
   include Secured # So this is private
   
-  @users = [
-    {"name": "Juan Manuel Mougan", "email": "juan@mymail.com", "is_admin": true},
-    {"name": "Jeff Bezos", "email": "jeff@amazon.com", "is_admin": false},
-    {"name": "Bill Gates", "email": "bill@microsoft.com", "is_admin": false}
-  ]
-  
   def list
-    render json: @users
+    users = [
+      {"name": "Juan Manuel Mougan", "email": "juan@mymail.com", "is_admin": true},
+      {"name": "Jeff Bezos", "email": "jeff@amazon.com", "is_admin": false},
+      {"name": "Bill Gates", "email": "bill@microsoft.com", "is_admin": false}
+    ]
+    render json: users
   end
 end
